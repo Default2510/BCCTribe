fe6f9898b3f9b596cb342262feeb19c43a952bd1
Robert Renaud <rr.renaud@gmail.com>
2015-07-28 18:22:03 -0500

    Use Google Calendar for events
    Change events page to pull data from Google Calendar instead of from a
    json file

--- Tribe/app/controllers/events.js
+++ Tribe/app/controllers/events.js
@@ -13,79 +13,80 @@
 		$rootScope.$broadcast("refreshHeader", {});
 		$window.ga('send', 'pageview', { page: $location.url() });
 		$scope.events = [];
-		$scope.maxEvents = 6;
-		$scope.seekFirstEvents = [];
+		$scope.recurringEvents = [];
 		$scope.eventToday = false;
+		$scope.today = new Date();
 		
-		// Load data in from json files using services
-		$http.get("app/data/events.json").
+		// $scope.today.setHours(0,0,0,0);
+		
+		// Load data in from Google Calendar in json format
+		var myKey = 'AIzaSyCR0TCWzOYEldfv3W_4cUvlyOjtXiNr6m0';
+		var calendarId = 'bcctribe@gmail.com';
+		var timeMin = $scope.today.toISOString();
+		var singleEvents = true;
+		var orderBy = "startTime";
+		var maxResults = 20;
+		var calendarBaseURL = 'https://www.googleapis.com/calendar/v3/calendars/' + calendarId+ '/events?key=' + myKey + '&timeMin=' + timeMin + '&singleEvents=' + singleEvents + '&orderBy=' + orderBy + '&maxResults=' + maxResults;
+		$http.get(calendarBaseURL).
 		success(function(data, status, headers, config) {
-			// This callback will be called asynchronously when the response is available
-			$scope.today = new Date();
-			$scope.today.setHours(0,0,0,0);	// Need to set hours to 00:00:00:00 for comparison of dates
-			// Add the events
-			var events = data.events;
+			var events = data.items;
 			for (var i = 0; i < events.length; ++i) {
-				// Add events that are equal to or greater than today and not further out than 30 days
 				var event = events[i];
-				var eventDate = new Date(event.date + "T00:00:00");
-				event.today = false;
-				if (eventDate >= $scope.today) {
-					// Add notification to event if the event is today
-					if (+eventDate == +$scope.today) {
-						event.today = true;
-						$scope.eventToday = true;
-					}
-					// Add notification to event if it is a Corporate gathering/service (Note: getDay() == 3 is Wednesday)
-					if (eventDate.getDay() == 3 && event.time == "7pm" && event.place == "Bellevue Christian Center") {
-						event.service = true;
-					}
-					$scope.events.push(event);
-					if ($scope.events.length >= $scope.maxEvents) {
-						break;
-					}
+				// If the event's summary contains "Tribe Gathering" add the service flag
+				if (event.summary.indexOf("Tribe Gathering") > -1) {
+					event.service = true;
 				}
-			}
-			// Add the Seek First events (For days of the week Sunday: 0, Monday: 1, etc.)
-			var seekFirstEvents = data.seekFirst;
-			for (var i = 0; i < seekFirstEvents.length; ++i) {
-				var event = seekFirstEvents[i];
-				// Only show 1 weeks worth at a time
-				for (var j = 0; j < 7; ++j) {
-					var date = new Date($scope.today);
-					date.setDate($scope.today.getDate()+j);
-					// If the day of the week matches the event's day of the week add the event to the array
-					if (date.getDay() == event.day) {
-						event = $scope.findNextAvailableSeekFirst(event, date);
-						$scope.seekFirstEvents.push(event);
-						break;
-					} else {
-						continue;
-					}
+				// If the event is today add the today flags
+				if ($scope.isToday(event.start.dateTime)) {
+					event.today = true;
+					$scope.eventToday = true;
+				}
+				// Only add events that do NOT have "Weekly" in the description field
+				if (event.description != "Weekly") {
+					$scope.events.push(event);
 				}
 			}
 		}).
 		error(function(data, status, headers, config) {
-			alert("Help! Error loading events data");
+			alert("Help! Error loading events data from Google API");
 			// called asynchronously if an error occurs or server returns response with an error status.
 		});
 		
-		// function to get the next Available Seek First Date for the given day of the week
-		$scope.findNextAvailableSeekFirst = function(event, date) {
-			var dateString = date.toISOString().slice(0, 10);
-			if (event.exclude.indexOf(dateString) < 0) {
-				event.date = date;
-				
-				// Add notification to event if the event is today
-				if (+date == +$scope.today) {
+		// Load data in from Google Calendar in json format for recurring events (Have "Weekly" in the event description)
+		var q = "Weekly";
+		$http.get(calendarBaseURL + '&q=' + q).
+		success(function(data, status, headers, config) {
+			var summarylist = [];
+			var events = data.items;
+			for (var i = 0; i < events.length; ++i) {
+				var event = events[i];
+				// If the event is today add the today flags
+				if ($scope.isToday(event.start.dateTime)) {
 					event.today = true;
 					$scope.eventToday = true;
 				}
-			} else {
-				date.setDate(date.getDate()+7);
-				$scope.findNextAvailableSeekFirst(event, date);
+				// Only add events that are NOT already on the summary list
+				if (summarylist.indexOf(event.summary) < 0) {
+					$scope.recurringEvents.push(event);
+					summarylist.push(event.summary);
+				}
 			}
-			return event;
+		}).
+		error(function(data, status, headers, config) {
+			alert("Help! Error loading recurring events data from Google API");
+			// called asynchronously if an error occurs or server returns response with an error status.
+		});
+			
+		// function to determine if a given date is today
+		$scope.isToday = function(event) {
+			var todayDate = new Date($scope.today).setHours(0, 0, 0, 0);
+			var eventDate = new Date(event).setHours(0, 0, 0, 0);
+			
+			if (todayDate == eventDate) {
+				return true;
+			} else {
+				return false;
+			}
 		};
 	}
 	
